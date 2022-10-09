e912049b813b739ea2f9795a7caf961665fec4ce
abdul1990 <abdulrasack0217@gmail.com>
2022-10-09 17:36:04 -0400

    Started app added temp time and buttons

---
+++ weather-the-storm/src/components/TimeAndLocation.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function TimeAndLocation() {
+  return <div>
+    <div className='flex items-center justify-center my-6'>
+        <p className='text-white text-xl font-extralight'>
+            Sunday, 9 October 2022 | Local Time: 04:26 PM
+        </p>
+    </div>
+
+    <div className='flex items-center justify-center my-3'>
+        <p className='text-white text-3xl font-medium'>Georgetown, GY</p>
+    </div>
+  </div>
+}
+
+export default TimeAndLocation
