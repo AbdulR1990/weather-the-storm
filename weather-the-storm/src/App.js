7d0d9d0dff762269ea279526bd6bd5a3c47fecd6
abdul1990 <abdulrasack0217@gmail.com>
2022-10-10 11:59:46 -0400

    Added functionality and applied for API

--- weather-the-storm/src/App.js
+++ weather-the-storm/src/App.js
@@ -4,16 +4,42 @@ import TopButtons from './components/TopButtons';
 import Inputs from './components/Inputs';
 import TimeAndLocation from './components/TimeAndLocation';
 import TemperatureAndDetails from './components/TemperatureAndDetails';
+import Forecast from './components/Forecast';
+import getFormattedWeatherData from './services/WeatherService';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [query, setQuery] = useState({q: 'london'});
+  const [units, setUnits] = useState('metric');
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      await getFormattedWeatherData({ ...query, units }).then(data => {
+        setWeather(data);
+      });
+    };
+  
+    fetchWeather();
+  }, [query, units]);
+
+
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br 
     from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
-      <TopButtons />
-      <Inputs />
+      <TopButtons setQuery={setQuery} />
+      <Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
 
-      <TimeAndLocation />
-      <TemperatureAndDetails />
+      {weather && (
+        <div>  
+          <TimeAndLocation weather={weather} />
+          <TemperatureAndDetails weather={weather} />
+
+          <Forecast title='Hourly Forecast' items={weather.hourly} />
+          <Forecast title='Daily Forecast' items={weather.daily} />
+        </div>  
+      )}
     </div>
   );
 }
--- weather-the-storm/src/components/TimeAndLocation.jsx
+++ weather-the-storm/src/components/TimeAndLocation.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { formatToLocalTime } from '../services/WeatherService';
 
-function TimeAndLocation() {
+function TimeAndLocation({weather: {dt, timezone, name, country}}) {
   return <div>
     <div className='flex items-center justify-center my-6'>
         <p className='text-white text-xl font-extralight'>
-            Sunday, 9 October 2022 | Local Time: 04:26 PM
+            {formatToLocalTime(dt, timezone)}
         </p>
     </div>
 
     <div className='flex items-center justify-center my-3'>
-        <p className='text-white text-3xl font-medium'>Georgetown, GY</p>
+        <p className='text-white text-3xl font-medium'>{`${name}, ${country}`}</p>
     </div>
   </div>
 }
 
-export default TimeAndLocation
+export default TimeAndLocation;
